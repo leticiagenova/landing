8e6b387cd89fb037f09ca6c37ee82a03306c290c
Letícia Gênova <leticia@mastop.com.br>
2017-09-12 10:00:20 -0300

    Menu Hamburger

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,3 +1,24 @@
-/**
- * Created by leticiagenova on 24/08/17.
- */
+///**
+// * Created by leticiagenova on 24/08/17.
+// */
+
+
+$( document ).ready(function() {
+
+    $( ".cross" ).hide();
+    $( ".menu" ).hide();
+    $( ".menu_hamburger" ).click(function() {
+        $( ".menu" ).slideToggle( "slow", function() {
+            $( ".menu_hamburger" ).hide();
+            $( ".cross" ).show();
+        });
+    });
+
+    $( ".cross" ).click(function() {
+        $( ".menu" ).slideToggle( "slow", function() {
+            $( ".cross" ).hide();
+            $( ".menu_hamburger" ).show();
+        });
+    });
+
+});
